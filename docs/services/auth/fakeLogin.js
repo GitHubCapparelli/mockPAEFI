9c7e4d19c2e61256a550fe73577a51cf6db7486e
Capparelli <kdcapparelli@hotmail.com>
2025-12-23 19:36:18 -0300

    siglas

--- docs/services/auth/fakeLogin.js
+++ docs/services/auth/fakeLogin.js
@@ -7,9 +7,18 @@ const fetchUnidades = async () => {
 };
  
 const fetchServidores = async () => {
+  const unidades = await fetchUnidades();
   const response = await fetch('/mockPAEFI/data/mock/usuariosServidores.json');
   const result   = await response.json();
-  return Array.isArray(result) ? result : result.usuariosServidores; 
+  const lista    = Array.isArray(result) ? result : result.usuariosServidores; 
+
+  return lista.map(s => {
+    const unit = unidades.find(u => u.id === s.unidadeID);
+    return {
+      ...s,
+      unidade: unit ? unit.sigla : 'Unidade não localizada'
+    }
+  });
 };
 
 export const Papel = Object.freeze({
@@ -26,8 +35,8 @@ const servidorCom = async(papel) => {
   console.log(papel);
   const data = await fetchServidores();
   switch (papel) {
-    case Papel.SUBSAS: return data.find(u => u.sigla === 'GERVIS' || u.sigla === 'SUBSAS');
-    case Papel.DISEFI: return data.find(u => u.sigla === 'DISEFI' || u.sigla === 'CPSM');
+    case Papel.SUBSAS: return data.find(s => s.unidade === 'GERVIS' || s.unidade === 'SUBSAS');
+    case Papel.DISEFI: return data.find(s => s.unidade === 'DISEFI' || s.unidade === 'CPSM');
     default: return null;
   }
   // TODO:
@@ -39,11 +48,9 @@ const getFakeUser = async(papel) => {
   const servidor  = await servidorCom(papel);
   if (!servidor) return null;
 
-  const units = await fetchUnidades();
-  const unit  = units.find(u => u.id === servidor.unidadeID);
   return {
     nome    : servidor.nome,
-    unidade : unit ? unit.sigla : 'Unidade não localizada',
+    unidade : servidor.unidade,
     login   : servidor.login,
     perfil  : papel
   };
