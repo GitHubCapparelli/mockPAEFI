9063f11adb7351f416ee49e21fd121f93baca606
Capparelli <kdcapparelli@hotmail.com>
2025-12-26 19:27:05 -0300

    JS contains (includes?)

--- docs/services/auth/fakeLogin.js
+++ docs/services/auth/fakeLogin.js
@@ -38,7 +38,7 @@ const fetchMappedServidores = async () => {
       ...servidor,
       unidade     : unit ? unit : 'Não localizada',
       hierarquia  : unit ? buildUnidadeHierarchy(unit, unidadesById) : 'Não localizada',
-      podeAcessar : escopo.contains(servidor.unidade) || unit.sigla.startsWith('CREAS')
+      podeAcessar : escopo.includes(servidor.unidade) || unit.sigla.startsWith('CREAS')
     }
   });
 };
