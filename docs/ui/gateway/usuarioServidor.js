4774f57865f4b25bc21536454a80767887f28778
Capparelli <kdcapparelli@hotmail.com>
2025-12-30 11:04:08 -0300

    align

--- docs/ui/gateway/usuarioServidor.js
+++ docs/ui/gateway/usuarioServidor.js
@@ -166,9 +166,9 @@ function renderActions() {
 function renderFilters(caption) {
   const $section = $(sectionFiltersID);
   $section.empty();
-  $section.addClass('filters-bar mx-5rem mt-3 d-flex flex-column justify-content-between align-items-end gap-3');
+  $section.addClass('filters-bar mx-5rem mt-3 d-flex flex-column justify-content-between gap-3');
   $section.append(`
-    <h3>${caption}</h3>
+    <h3 class="w-100 my-2">${caption}</h3>
     <div class="w-100 simple-border d-flex flex-column flex-wrap gap-1">
       <div class="filter-options w-100 p-2 d-flex gap-3 flex-nowrap">
         <div class="filter-item">
