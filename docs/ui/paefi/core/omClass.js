88ab1348ed6e4294fd767c589ec9cbcb723de7db
Capparelli <kdcapparelli@hotmail.com>
2026-01-14 16:07:53 -0300

    command console.error(e);

--- docs/ui/paefi/core/omClass.js
+++ docs/ui/paefi/core/omClass.js
@@ -69,72 +69,8 @@ export class CommandEngine {
       return true;
     } catch (e) {
       this.onError?.(e);
+      console.error(e);
       return false;
     }
-  }
-}
-
-export class Modal { 
-  constructor(id, title, onSaveRequested) {
-    this.id             = id;
-    this.title          = title;
-    this.saveRequested  = onSaveRequested;
-    
-    this.bodyElement    = null;
-    this.init();
-  }
-
-  init() {
-    this.renderStructure();
-    $('#btnConfirm').on('click', () => this.submit());
-  }
-
-    renderStructure() {
-        const $modalRoot = $('#modal-root').empty();
-        const $modal     = $('<div>', { class: 'modal fade', id: this.id });
-        const $dialog    = $('<div>', { class: 'modal-dialog modal-lg' });
-        const $content   = $('<div>', { class: 'modal-content' });
-        
-        const $header    = $('<div>', { class: 'modal-header' })
-          .append($('<h5>', { class: 'modal-title', text: this.title }) );
-        
-        const $body      = $('<div>', { id: 'modal-body', class: 'modal-body' })
-          .append($('<span>', { class: 'text-mute', text: '[form goes here...]' }) );
-
-        const $footer = $('<div>', { class: 'modal-footer' })
-          .append(
-            $('<button>', { class: 'btn btn-secondary', type: 'button', 'data-bs-dismiss': 'modal', text: 'Cancelar' })
-            ,
-            $('<button>', { class: 'btn btn-primary', id: 'btnConfirm', text: 'Salvar' })
-        );
-
-        $content.append($header, $body, $footer);
-        $dialog.append($content);
-        $modal.append($dialog);
-        $modalRoot.append($modal);  
-
-        this.bodyElement = $('#modal-body');
-        return $body;
-    }
-  
-  open(data = null) {
-    this.render(data);
-    $(`#${this.id}`).modal('show');
-  }
-  close()       { $(`#${this.id}`).modal('hide'); }
-
-  collect()     { throw new Error('collect() must be implemented'); }
-  render(data)  { throw new Error('render() must be implemented'); }
-
-  async submit(id = null) {
-    const payload = this.collect();
-
-  this.saveRequested(payload);
-
-//    const ok = (this.mode === 'create')
-//             ? await this.command.Create(payload)
-//             : await this.command.Update(id, payload);
-//
-//    if (ok) this.close();
   }
 }
