bacc3958d930b5b4807d3fe7602336b648b55254
Capparelli <kdcapparelli@hotmail.com>
2026-01-15 09:23:25 -0300

    <div>

--- docs/ui/paefi/core/omModal.js
+++ docs/ui/paefi/core/omModal.js
@@ -210,7 +210,7 @@ export class ModalMessageBuilder {
     renderBody($container) {
         $container.empty().addClass('d-flex align-items-center justify-content-center text-center');
         if (this.message.startsWith('<')) {
-            const $msgBody = $('div', {id: 'msg-body', class: 'msg-body' }).html(this.message);
+            const $msgBody = $('<div>', { id: 'msg-body', class: 'msg-body', html: this.message });
             $container.append($msgBody);
         } else {
             $container.append($('<p>', { class: 'mb-0 fs-5', text: this.message }));
