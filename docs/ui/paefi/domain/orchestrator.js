fc5c37d659ef03f33f69eb40b95e677e8f27c1e7
Capparelli <kdcapparelli@hotmail.com>
2026-01-26 12:23:25 -0300

    err

--- docs/ui/paefi/domain/orchestrator.js
+++ docs/ui/paefi/domain/orchestrator.js
@@ -160,9 +160,9 @@ export class Orchestrator {
     }
 
     async #messageOnError(response, title) {
-        if (response?.error) {
+        //if (response?.error) {
             const builder = ModalMessageBuilder.Create(title, response.error);
             await this.modal.open(builder);
-        }
+        //}
    }
 }
