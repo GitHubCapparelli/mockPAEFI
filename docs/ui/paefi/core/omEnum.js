a923e1315a4f36bdfc9ca837d0e62fcd6664c17d
Capparelli <kdcapparelli@hotmail.com>
2026-02-09 15:18:29 -0300

    denúncias

--- docs/ui/paefi/core/omEnum.js
+++ docs/ui/paefi/core/omEnum.js
@@ -88,7 +88,7 @@ export class Dominio extends BaseEnum {
     static Compromissos         = new Dominio('compromissos', 'Compromissos');
     static Database             = new Dominio('catalogos', 'Database (tabelas)');
     static Demandas             = new Dominio('demandas','Demandas');
-    static Denuncias            = new Dominio('denuncias', 'Denúncias');
+    static Denuncias            = new Dominio('denuncias', 'Registros de violação');
     static Documentos           = new Dominio('documentos','Documentos');
     static Enderecos            = new Dominio('enderecos', 'Endereços');
     static Historico            = new Dominio('historico', 'Histórico de eventos');
