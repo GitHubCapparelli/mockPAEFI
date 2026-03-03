9471e1084b9ee1ad3719f9d7136a415d33d27b8e
Capparelli <kdcapparelli@hotmail.com>
2026-03-03 13:45:55 -0300

    migrando packages data

---
+++ packages/data/repositories/historico.js
@@ -0,0 +1,27 @@
+// packages/data/repositories/historicoRepository.js
+import { BaseRepository } from './_baseRepo.js';
+
+export class HistoricoRepository extends BaseRepository {
+
+    constructor() {
+        super('historico', {
+            defaultOrderBy: 'dataHora',
+            filterableColumns: ['usuarioID', 'catalogoID', 'tipo', 'acao']
+        });
+    }
+
+    // Sobrescreve para deixar explícito que histórico é imutável
+    update() {
+        throw new Error('Registros históricos não podem ser alterados.');
+    }
+
+    softDelete() {
+        throw new Error('Registros históricos não podem ser excluídos.');
+    }
+
+    hardDelete() {
+        throw new Error('Registros históricos não podem ser excluídos.');
+    }
+}
+
+export default new HistoricoRepository();
